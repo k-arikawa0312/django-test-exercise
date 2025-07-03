e23fdeabd1e7314bd1d62b7555bca57f41c67825
k-arikawa0312 <s1f102402710@iniad.org>
2025-07-03 11:42:47 +0900

    トップページを追加

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.timezone import make_aware
+from django.utils.dateparse import parse_datetime
+from todo.models import Task
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST':
+        task=Task(title=request.POST['title'],due_at=parse_datetime(request.POST['due_at']))
+        task.save()
+    
+    tasks = Task.objects.all()
+
+    context = {
+        'tasks': tasks,
+    }
+
+    return render(request, 'todo/index.html', context)
